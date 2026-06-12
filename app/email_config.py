819506468c1f7c64e5a9140b7464424bc1e45ff5
yazid32 <imcomingfor.it.all23@gmail.com>
2026-06-12 11:44:17 +0100

    why the change

--- app/email_config.py
+++ app/email_config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def send_email(to: str, subject: str, html: str):
-    resend.api_key = os.getenv("RESEND_API_KEY")
+    resend.api_key = os.getenv("re_ALbrQtBX_2DPuEpoJK5ze3bgEhszfD4am")
     if not resend.api_key:
         raise Exception("RESEND_API_KEY is not set")
     return resend.Emails.send({
