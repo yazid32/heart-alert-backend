0f5dbb00ed94cab8a3bcf33156788c5699ee585f
yazid32 <imcomingfor.it.all23@gmail.com>
2026-06-13 11:06:29 +0100

    fix: redirect web users to reset password page

--- app/main.py
+++ app/main.py
@@ -2,8 +2,7 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
-from fastapi import FastAPI, Depends, HTTPException, status, Query, UploadFile, File
+from fastapi import FastAPI, Depends, HTTPException, status, Query, UploadFile, File, Header
 from fastapi.responses import HTMLResponse
 import shutil
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
